56905f581320737abf5ff26ac388e4c5c1fa871f
Grandperhaps <45555670+Grandperhaps@users.noreply.github.com>
2019-07-26 19:41:40 +0800

    Update rank_asc_spec.js

--- spec/map/rank_asc_spec.js
+++ spec/map/rank_asc_spec.js
@@ -9,4 +9,18 @@ describe('rank by asc', function() {
     var result = rank_asc(collection_a);
     expect(result).toEqual(collection_b);
   });
-});
+}); 
+var collection_a[10],collection_b[10];
+ for ( var i=0;i<collection_a.length-1;i++){       
+      for (var j=0;j<collection_a.length-1-i;j++) {     
+            if (collection_a[j] > collection_a[j + 1]) {           
+                 var temp = collection_a[i];            
+                     collection_a[j] = collection_a[j + 1];      
+                               collection_a[j + 1] = temp;        
+                                   }     
+                               }  
+                   }
+                   if(var o = 0 ; o <= collection_a.length-1 ; o++){
+                   	collection_a[o]=collection_b[o];
+                   }
+                   console.log(collection_b);
